ae543ef93697c3fdb52c7ea60a19eae045aa1da9
Zak Hargreaves <zak@hargreaves.xyz>
2026-03-27 20:12:22 +0000

    Add ait formula v0.3.0

---
+++ Formula/ait.rb
@@ -0,0 +1,30 @@
+# typed: false
+# frozen_string_literal: true
+
+class Ait < Formula
+  desc "AI Toolkit Package Manager - npm for AI agents, skills, and prompts"
+  homepage "https://github.com/ZakHargz/ait"
+  url "https://github.com/ZakHargz/ait/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "7f1aca9a41317ad161c277d248fbb609123faf81e0eaa249914930d89c6ac224"
+  license "MIT"
+  head "https://github.com/ZakHargz/ait.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/ait"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ait --version")
+    
+    # Test init command
+    system bin/"ait", "init", "--defaults"
+    assert_predicate testpath/"ait.yml", :exist?
+    
+    # Verify ait.yml content
+    ait_yml = File.read(testpath/"ait.yml")
+    assert_match "name:", ait_yml
+    assert_match "version:", ait_yml
+  end
+end
